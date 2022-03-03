0e3d5ffdb211e42000e86b08c1cec3939c67be2d
analuizazevedo <ana.luiza@geb.inatel.br>
2022-03-03 18:29:31 -0300

    Resolvendo o Exercicio 1

---
+++ src/main/java/Exercicio1.java
@@ -0,0 +1,13 @@
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+
+        int t1 = 130;
+        int t2 = 240;
+        int t3 = 320;
+        int t = t1 + t2 + t3;
+
+        System.out.println("Total Zumbies: " + t);
+        System.out.println("Média Zumbies: " + t / 3);
+    }
+}
